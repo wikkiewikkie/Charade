0a8548f52b59d49a1b2bcde167be3b84ffd65521
Kevin Schellenberg <wikkiewikkie@gmail.com>
2017-01-11 22:49:17 -0500

    implement additional tests

--- tests/test_charade.py
+++ tests/test_charade.py
@@ -1,14 +1,14 @@
 import pytest
 
-from charade import Charade
+from charade import Charade, Email, EmailAttachment, File, Image
 
 
 @pytest.fixture
-def config():
-    return {}
+def instance():
+    return Charade(prefix="TEST", fill=5, start=11, size=5000)
 
 
-def test_charade_init():
+def test_charade():
     c = Charade(prefix="TEST", fill=5, start=11, size=50)
     assert "Charade(" in repr(c)
     assert len(c.people) >= 15  # minimum of 15 people, even for small sets
@@ -17,3 +17,23 @@ def test_charade_init():
     for d in c:
         count += 1 + len(d)
     assert count >= 5000  # returns at least number specified in size
+
+
+def test_email(instance):
+    e = Email(instance)
+    assert "Email(" in repr(e)
+    assert "From:" in str(e)
+    for a in e:
+        assert isinstance(a, EmailAttachment)
+    assert "EmailAttachment(" in repr(a)
+    assert len(a) == 1
+
+
+def test_file(instance):
+    f = File(instance)
+    for a in f:
+        assert isinstance(a, File)
+    assert "File(" in repr(f)
+
+def test_image(instance):
+    i = Image(instance)
